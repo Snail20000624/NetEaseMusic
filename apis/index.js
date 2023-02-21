2dbe95bfd2c675e4a6154e7d81f3d95803c155e5
Liam <1250919453@qq.com>
2023-02-21 13:28:14 +0800

    update: 首页静态界面开发

--- apis/index.js
+++ apis/index.js
@@ -1,10 +1,43 @@
 import request from "@/utils/request.js"
 
 // 轮播图请求接口
-export function apiGetBanner(data) {
+const apiGetBanner = (data) => {
   return request.request({
     url: '/banner',
     method: 'GET',
     data
   })
+}
+
+// 推荐歌单
+const apiGetRecommendSongs = (data) => {
+  return request.request({
+    url: '/personalized',
+    method: 'GET',
+    data
+  })
+}
+
+// 新碟 / 新歌
+const apiGetTopAlbum = (data) => {
+  return request.request({
+    url: '/album/newest',
+    method: 'GET',
+    data
+  })
+}
+
+// 精选视频
+const apiGetRelatedVideo = (data) => {
+  return request.request({
+    url: '/related/allvideo',
+    method: "GET",
+    data
+  })
+}
+export default {
+  apiGetBanner,
+  apiGetRecommendSongs,
+  apiGetTopAlbum,
+  apiGetRelatedVideo
 }
